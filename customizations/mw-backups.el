00985cafb724d4f31dbd61a03df5f5c156677382
Ali Razeen <alirazeen@alirazeen.com>
2016-12-13 20:27:34 -0500

    Remove unneeded file

--- customizations/mw-backups.el
+++
@@ -1,5 +0,0 @@
-;; store all backup and autosave files in the tmp dir
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
