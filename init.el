8a0d1ea020e055e2b76421795b3a46bc68c448b9
Ali Razeen <alirazeen@alirazeen.com>
2016-12-13 20:32:38 -0500

    Some aesthetics fixing in init.el

--- init.el
+++ init.el
@@ -7,16 +7,17 @@
 
 (setq exec-path (append exec-path '("/usr/local/bin")))
 
-
 (require 'mw-initial-setup)
 (require 'mw-package-setup)
 (require 'mw-run-customizations)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(inhibit-startup-screen t))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
