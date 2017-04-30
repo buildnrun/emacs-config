98a76cabbfc5400fc6dcc41f2965f8510e3c3084
Ali Razeen <alirazeen@alirazeen.com>
2017-04-30 14:42:09 -0400

    Update package-selected-packages in init.el

--- init.el
+++ init.el
@@ -35,7 +35,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inhibit-startup-screen t))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (ztree zenburn-theme yaml-mode xcscope volatile-highlights undo-tree sml-mode smartrep smartparens scala-mode rainbow-mode rainbow-delimiters projectile paredit operate-on-number markdown-mode magit helm-ag guru-mode groovy-mode gradle-mode flx expand-region ess easy-kill diminish diff-hl counsel browse-kill-ring better-defaults auto-dictionary auto-complete-clang-async auto-complete auctex anzu))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
